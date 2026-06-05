4545d3ea396e1a426fd08db618cfe162168bb66f
Dennis Herrera <dennishumher25@hotmail.com>
2026-06-05 15:38:27 -0600

    chore: applied color scheme light only

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,8 @@
         <meta http-equiv="Last-Modified" content="0">
         <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
         <meta http-equiv="Pragma" content="no-cache">
+        <meta name="color-scheme" content="light only">
+        <meta name="theme-color" content="#ffffff">
 
         <title>{{ config('app.name', 'Quiniela') }}</title>
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="color-scheme" content="light only">
+        <meta name="theme-color" content="#ffffff">
 
         <title>{{ config('app.name', 'Quiniela') }}</title>
 
